50b03b504fdf69fb373b439e8385992803f529d9
Dannyflores16 <dannyflores787@gmail.com>
2026-07-28 22:44:45 -0600

    Guardo avances practica 2 Cloud Native

---
+++ performance/test.js
@@ -0,0 +1,18 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+  vus: 50,
+  duration: '30s',
+};
+
+export default function () {
+
+  const res = http.get('http://localhost:18080');
+
+  check(res, {
+    'status es 200': (r) => r.status === 200,
+  });
+
+  sleep(1);
+}
--- src/app.js
+++ src/app.js
@@ -1,14 +1,31 @@
 console.log("Estoy ejecutando app.js");
 
 const express = require("express");
+const client = require("prom-client");
 
 const app = express();
 
-const PORT = 4000;;
+const PORT = 4000;
+
+// Métricas por defecto de Node.js
+client.collectDefaultMetrics();
+
+// Contador de peticiones HTTP
+const httpRequests = new client.Counter({
+    name: "http_requests_total",
+    help: "Número total de peticiones HTTP"
+});
+
+app.use((req, res, next) => {
+    httpRequests.inc();
+    next();
+});
+
 
 app.get("/", (req, res) => {
     res.send("🚀 Practica 2 - Cloud Native funcionando correctamente");
 });
+
 
 app.get("/health", (req, res) => {
     res.status(200).json({
@@ -16,6 +33,14 @@ app.get("/health", (req, res) => {
         message: "Servicio funcionando correctamente"
     });
 });
+
+
+// Endpoint para Prometheus
+app.get("/metrics", async (req, res) => {
+    res.set("Content-Type", client.register.contentType);
+    res.end(await client.register.metrics());
+});
+
 
 app.listen(PORT, () => {
     console.log(`Servidor ejecutándose en el puerto ${PORT}`);
